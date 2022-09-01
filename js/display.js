d014d9630f1384064a29cc2d4ea3cb2767f9487f
Valentin Airaldo <valentin.airaldo15@gmail.com>
2022-09-01 18:55:52 -0300

    mostrar mail y asignar categoria
    se muestra el mail en la barra de navegacion y se asigna los productos a la categoria

---
+++ js/display.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
+
+var Usuario =  localStorage.getItem("Usuario")
+
+function getHTML(){
+   return `<li class="nav-item">
+   <a class="nav-link" href="">${Usuario}</a>
+ </li>`
+}
+
+document.addEventListener("DOMContentLoaded", async function(){
+    const navbar = document.querySelector("#navBar")
+    
+    navbar.innerHTML += getHTML();  
+})
